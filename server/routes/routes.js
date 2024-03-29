ec9d387e82c728e054e4333940995d41137439fa
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-03-29 18:28:26 -0500

    Se anade middleware de autenticacion

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import ProductController from '../controllers/ProductController.js'
 import { LoginController } from '../controllers/LoginController.js'
+import validateAccesToken from '../middlewares/auth.middleware.js'
 
 export const router = Router()
 
-router.get('/', ProductController.getAllProducts)
-router.get('/:id', ProductController.getProductById)
-router.post('/create', ProductController.createProduct)
-router.patch('/update/:id', ProductController.updateProduct)
-router.delete('/delete/:id', ProductController.deleteProduct)
+router.get('/', validateAccesToken, ProductController.getAllProducts)
+router.get('/:id', validateAccesToken, ProductController.getProductById)
+router.post('/create', validateAccesToken, ProductController.createProduct)
+router.patch('/update/:id', validateAccesToken, ProductController.updateProduct)
+router.delete('/delete/:id', validateAccesToken, ProductController.deleteProduct)
 router.post('/login', LoginController.login)
