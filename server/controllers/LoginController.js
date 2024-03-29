2d1c99ddc2f6b0b15e65ee905a861ea66e307031
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-03-29 18:26:29 -0500

    Se anade envio de token al login correcto

--- server/controllers/LoginController.js
+++ server/controllers/LoginController.js
@@ -1,17 +1,37 @@
+import { JWT_EXPIRES, JWT_SECRET } from '../config/envVariables.js'
 import LoginModel from '../models/Login.js'
+import { InvalidCredential, NotFoundUser } from '../schemas/errorSchema.js'
+import jwt from 'jsonwebtoken'
 
 export class LoginController {
   static async login (req, res) {
     const { userName, userPassword } = req.body
 
     const response = await LoginModel.login({ userName, userPassword })
-    if (response instanceof Error) {
-      res.status(500).json({ message: 'Error interno del servidor ' })
-    } else {
-      res.json({
-        success: true,
-        message: 'Inicio de sesion exitoso'
+    if (response instanceof NotFoundUser) {
+      return res.status(404).json({
+        message: 'No existe este usuario',
+        success: false
       })
     }
+    if (response instanceof InvalidCredential) {
+      return res.status(401).json({
+        message: 'Contrasena incorrecta',
+        success: false
+      })
+    }
+    if (response instanceof Error) {
+      return res.status(500).json({
+        message: 'ha ocurrido un error al iniciar sesion',
+        success: false
+      })
+    }
+
+    const accessToken = jwt.sign({ userName }, JWT_SECRET, { expiresIn: JWT_EXPIRES })
+    res.json({
+      success: true,
+      message: 'Inicio de sesion exitoso',
+      accessToken
+    })
   }
 }
--- server/models/Login.js
+++ server/models/Login.js
@@ -1,23 +1,24 @@
 import db from '../config/dataBase.js'
+import { InvalidCredential, NotFoundUser } from '../schemas/errorSchema.js'
 
 export default class LoginModel {
   static async login ({ userName, userPassword }) {
     try {
-      const [usuario] = await db.query(
+      const [[usuario]] = await db.query(
         'SELECT nombre_usuario, contrasena_usuario FROM users WHERE nombre_usuario = ?',
         [userName]
       )
-      if (!usuario) throw new Error()
-      if (usuario.length === 0) throw new Error()
+      if (!usuario) throw new NotFoundUser()
+      if (usuario.length === 0) throw new NotFoundUser()
 
       const validacion = () => {
         if (userPassword === usuario.contrasena_usuario) return true
         return false
       }
 
-      if (!validacion) throw new Error()
+      if (!validacion) throw new InvalidCredential()
 
-      return usuario[0]
+      return usuario
     } catch (err) {
       return err
     }
