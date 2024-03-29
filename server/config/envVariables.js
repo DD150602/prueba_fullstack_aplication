b8b5f2992c737e3306271047cf1766bff5eacfbe
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-03-29 18:29:15 -0500

    Se anaden nuevas variables para el funcionamiento

--- server/config/envVariables.js
+++ server/config/envVariables.js
@@ -6,3 +6,6 @@ export const USER = process.env.DATABASE_USER
 export const DATABASE_PORT = process.env.DATABASE_PORT
 export const PASSWORD = process.env.DATABASE_PASSWORD
 export const DATABASE_NAME = process.env.DATABASE_NAME
+export const JWT_SECRET = process.env.JWT_SECRET
+export const JWT_EXPIRES = process.env.JWT_EXPIRES
+export const JWT_COOKIE_EXPIRES = process.env.JWT_COOKIE_EXPIRES
